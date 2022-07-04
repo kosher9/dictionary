5d280d778b8c241bd0cd6e83dbc110614b2fa52b
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-04 00:25:26 +0000

    Add go.mod file

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"dictionnary/dictionary"
+	"dictionary/dictionary"
 	"flag"
 	"fmt"
 	"os"
 )
 
-func main(){
+func main() {
 
 	action := flag.String("action", "list", "Action to perform")
 
@@ -30,7 +30,7 @@ func main(){
 	}
 }
 
-func actionAdd(d *dictionary.Dictionary, args []string)  {
+func actionAdd(d *dictionary.Dictionary, args []string) {
 	word := args[0]
 	definition := args[1]
 	err := d.Add(word, definition)
@@ -38,30 +38,30 @@ func actionAdd(d *dictionary.Dictionary, args []string)  {
 	fmt.Printf("\n'%v' added to the dictionary\n", word)
 }
 
-func actionList(d *dictionary.Dictionary){
+func actionList(d *dictionary.Dictionary) {
 	words, entries, err := d.List()
 	handleError(err)
 	fmt.Println("\nDictionary content\n")
-	for _, word := range words{
+	for _, word := range words {
 		fmt.Println(entries[word])
 	}
 }
-func actionDefine(d *dictionary.Dictionary, args []string)  {
+func actionDefine(d *dictionary.Dictionary, args []string) {
 	word := args[0]
 	entry, err := d.Get(word)
 	handleError(err)
 	fmt.Println(entry)
 }
 
-func actionRemove(d *dictionary.Dictionary, args []string)  {
+func actionRemove(d *dictionary.Dictionary, args []string) {
 	word := args[0]
 	err := d.Remove(word)
 	handleError(err)
 	fmt.Printf("\n'%v' has been removed\n", word)
 }
 
-func handleError(err error){
-	if err != nil{
+func handleError(err error) {
+	if err != nil {
 		fmt.Printf("Dictionary error: %v", err)
 		os.Exit(1)
 	}
